a318703d52bb72092a76ff2ce9e3cd3224f2a191
Christian Reyes <cmreyesqla598@gmail.com>
2019-11-03 19:29:15 -0800

    events and todo touch ups

--- src/App.js
+++ src/App.js
@@ -10,8 +10,32 @@ class App extends Component {
         { description: "Walk the cat", isCompleted: true },
         { description: "Throw the dishes away", isCompleted: false },
         { description: "Buy new dishes", isCompleted: false }
-      ]
+      ],
+      newToDoDescription: ""
     };
+  }
+  toggleComplete(index) {
+    const todos = this.state.todos.slice();
+    const todo = todos[index];
+    todo.isCompleted = todo.isCompleted ? false : true;
+    this.setState({ todos: todos });
+  }
+  handleSubmit(e) {
+    e.preventDefault();
+    if (!this.state.newToDoDescription) {
+      return;
+    }
+    const newToDo = {
+      description: this.state.newToDoDescription,
+      isCompleted: false
+    };
+    this.setState({
+      todos: [...this.state.todos, newToDo],
+      newToDoDescription: ""
+    });
+  }
+  handleChange(e) {
+    this.setState({ newToDoDescription: e.target.value });
   }
   render() {
     return (
@@ -22,9 +46,19 @@ class App extends Component {
               key={index}
               description={todo.description}
               isCompleted={todo.isCompleted}
+              toggleComplete={() => this.toggleComplete(index)}
             />
           ))}
         </ul>
+        <form onSubmit={e => this.handleSubmit(e)}>
+          <input
+            type="text"
+            value={this.state.newToDoDescription}
+            onChange={e => this.handleChange(e)}
+          />
+
+          <input type="submit" />
+        </form>
       </div>
     );
   }
